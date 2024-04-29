c3122890a7ca92ea72a864d22eba91d98796bf55
nimmat <48224993+nimmat@users.noreply.github.com>
2024-04-29 23:22:14 +0100

    Update staging.sh

--- staging.sh
+++ staging.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+sudo rm /boot/fullpageos.txt && mv fullpageos.txt /boot
+xrandr --addmode HDMI-2 1280x720
+xrandr --output HDMI-2 --mode 1280x720
+rm ../scripts/start_gui
+mv start_gui ../scripts/
+sudo chmod +x ../scripts/start_gui
