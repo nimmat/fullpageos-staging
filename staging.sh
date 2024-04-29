b2ada14c63535bd59ab9c8fbb92ed1270b41c5da
nimmat <48224993+nimmat@users.noreply.github.com>
2024-04-30 00:48:12 +0100

    Update staging.sh

--- staging.sh
+++ staging.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo rm /boot/fullpageos.txt && mv fullpageos.txt /boot
+sudo rm /boot/fullpageos.txt && sudo mv fullpageos.txt /boot
 xrandr --addmode HDMI-2 1280x720
 xrandr --output HDMI-2 --mode 1280x720
-rm ../scripts/start_gui
-mv start_gui ../scripts/
+sudo rm ../scripts/start_gui
+sudo mv start_gui ../scripts/
 sudo chmod +x ../scripts/start_gui
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -12,7 +12,7 @@ cd $HOME
 git clone https://github.com/Linkek/keyboardTriggerServer.git
 cd keyboardTriggerServer
 npm install
-cd ..
+cd $HOME/fullpageos-staging
 sudo mv keyboardTriggerServer.service /etc/systemd/system/
 sudo systemctl start keyboardTriggerServer
 sudo systemctl enable keyboardTriggerServer
