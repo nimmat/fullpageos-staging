c609641d85129d21f194efc701d74ade1fdaaa28
nimmat <48224993+nimmat@users.noreply.github.com>
2024-04-29 23:39:35 +0100

    Update staging.sh

--- staging.sh
+++ staging.sh
@@ -5,3 +5,10 @@ xrandr --output HDMI-2 --mode 1280x720
 rm ../scripts/start_gui
 mv start_gui ../scripts/
 sudo chmod +x ../scripts/start_gui
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install matchbox-keyboard nodejs npm -y
+cd $HOME
+git clone https://github.com/Linkek/keyboardTriggerServer.git
+cd keyboardTriggerServer
+npm install
